d716f39efa865144d0da59097e34df7c95599c54
dshchukin <dshchukin@mail.ru>
2016-04-20 17:15:08 +0300

    templates and views were added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Denis' } # user's nickname example
+    competitions = [ # competitons for loop example in template
+    	{
+    		'name': 'Competiton 1',
+    		'date': '21/04/2016'
+    	},
+    	{
+    		'name': 'Competiton 2',
+    		'date': '22/04/2016'
+    	}
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        competitions = competitions)
